2a101097ea70f2a9a90745ba869fd77c6b0c215a
Fidelis Felipe <atosfiel@gmail.com>
2021-06-11 21:00:30 -0300

    add cache 10s

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -4,6 +4,9 @@ async function tempo(request, response){
     const subscriberResponse = await fetch(`https://api.convertkit.com/v3/account?api_secret=${apiSecret}`);
     const subscriberResponseJson = await subscriberResponse.json();
     const email = subscriberResponseJson.primary_email_address;
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
     response.json({
         date: dynamicDate.toGMTString(),
         email: email
